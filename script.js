6e6f0e1e14baaf6a630ecc35366abe27f05fc18e
raquelaamendes <raquelalexandramendes@gmail.com>
2024-03-18 19:50:28 +0000

    Grid Creation
    I updated the code to automate the grid's generation.

---
+++ script.js
@@ -0,0 +1,27 @@
+const rows = 10;
+const cols = 10;
+const grid = [];
+
+const gridContainer = document.getElementById('grid-container');
+
+for (let i = 0; i < rows; i++) {
+    
+    grid[i] = [];
+    
+    for (let j = 0; j < cols; j++) {
+        
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        
+       
+        const cellId = `cell-${i}-${j}`;
+        cell.setAttribute('id', cellId);
+
+        const cellClass = `position`;
+        cell.setAttribute('class', cellClass);
+        
+        
+        gridContainer.appendChild(cell);
+        grid[i][j] = cell;
+    }
+}
